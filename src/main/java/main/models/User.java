57cdf247666c92f21586b838f203420a36e162e5
Sebastian Schmidt <schmidtseb@outlook.de>
2018-06-18 18:27:21 +0200

    Added user.java

---
+++ src/main/java/main/models/User.java
@@ -0,0 +1,43 @@
+package main.models;
+
+public class User {
+
+    private String username;
+    private String password;
+    private String name, surname;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+
+
+}
